2e24be5777e34b7b491b9453a38d889ca828f73d
Olena Pupova <olena@backbase.com>
2018-03-22 08:30:14 +0100

    websocket. implementation

--- src/main/java/com/ing/cashual/chatboting/connector/mattermost/MattermostWebSocketConnector.java
+++ src/main/java/com/ing/cashual/chatboting/connector/mattermost/MattermostWebSocketConnector.java
@@ -1,13 +1,13 @@
 package com.ing.cashual.chatboting.connector.mattermost;
 
-import org.springframework.messaging.converter.MappingJackson2MessageConverter;
-import org.springframework.messaging.simp.stomp.StompSessionHandler;
-import org.springframework.stereotype.Component;
-import org.springframework.web.socket.client.WebSocketClient;
-import org.springframework.web.socket.client.standard.StandardWebSocketClient;
-import org.springframework.web.socket.messaging.WebSocketStompClient;
 
-import java.util.Scanner;
+import com.google.gson.Gson;
+import com.ing.cashual.chatboting.connector.mattermost.json.MattermostEvent;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 /**
  * Created by oleg_pupov on 3/22/18.
@@ -17,16 +17,60 @@ import java.util.Scanner;
 @Component
 public class MattermostWebSocketConnector {
 
-    public void connect() {
-        WebSocketClient client = new StandardWebSocketClient();
+    private WebsocketClientEndpoint clientEndPoint;
 
-        WebSocketStompClient stompClient = new WebSocketStompClient(client);
-        stompClient.setMessageConverter(new MappingJackson2MessageConverter());
+    @PostConstruct
+    private void connect() {
 
-        StompSessionHandler sessionHandler = new MyStompSessionHandler();
-        stompClient.connect("ws://localhost:8065/api/v4/websocket", sessionHandler);
+        try {
+            // open websocket
+            clientEndPoint = new WebsocketClientEndpoint(new URI("ws://localhost:8065/api/v4/websocket"));
 
-        new Scanner(System.in).nextLine(); // Don't close immediately.
+            // add listener
+            clientEndPoint.addMessageHandler(new WebsocketClientEndpoint.MessageHandler() {
+                public void handleMessage(String message) {
+
+                    String[] tokens = message.split(",");
+
+                    String messageString = null;
+                    for (String token:tokens) {
+                        if (token.contains("\\\"message\\\":")) {
+                            messageString = token;
+                            messageString = messageString.substring(14,messageString.length()-2);
+                            break;
+                        }
+                    }
+
+
+
+                    Gson gson = new Gson();
+                    MattermostEvent mattermostEvent = gson.fromJson(message, MattermostEvent.class);
+
+                    if ("posted".equals(mattermostEvent.getEvent())) {
+                        System.out.println(messageString);
+                    }
+
+                }
+            });
+
+            // send message to websocket
+            clientEndPoint.sendMessage(
+                    "{\n" +
+                            "  \"seq\": 1,\n" +
+                            "  \"action\": \"authentication_challenge\",\n" +
+                            "  \"data\": {\n" +
+                            "    \"token\": \"pre3anp4c3dw5n8wubbu9tz4ja\"\n" +
+                            "  }\n" +
+                            "}");
+
+        } catch (URISyntaxException ex) {
+            System.err.println("URISyntaxException exception: " + ex.getMessage());
+        }
+
+    }
+
+    public void addMessageHandler(WebsocketClientEndpoint.MessageHandler messageHandler) {
+        clientEndPoint.addMessageHandler(messageHandler);
     }
 
 }
---
+++ src/main/java/com/ing/cashual/chatboting/connector/mattermost/json/MattermostEvent.java
@@ -0,0 +1,26 @@
+package com.ing.cashual.chatboting.connector.mattermost.json;
+
+/**
+ * Created by oleg_pupov on 3/22/18.
+ */
+public class MattermostEvent {
+    private String event;
+//    private Data data;
+
+    public String getEvent() {
+        return event;
+    }
+
+    public void setEvent(String event) {
+        this.event = event;
+    }
+
+//    public Data getData() {
+//        return data;
+//    }
+//
+//    public void setData(Data data) {
+//        this.data = data;
+//    }
+
+}
